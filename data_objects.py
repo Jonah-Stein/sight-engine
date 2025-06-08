7ca41fcb221a0db2f3982efbd302810017ab1ee0
Jonah Stein <jonah.s.stein@gmail.com>
2025-06-07 23:57:02 -0400

    calculate max sight points

--- data_objects.py
+++ data_objects.py
@@ -1,5 +1,6 @@
 from typing import NamedTuple
-
+from datetime import datetime
+from enums import AircraftSide
 class FlightSummaryRequest(NamedTuple):
     day: int
     month: int
@@ -9,3 +10,19 @@ class FlightSummaryRequest(NamedTuple):
     outbound_airport: str = None
     inbound_airport: str = None
 
+class TrackingPoint(NamedTuple):
+    timestamp: datetime
+    lat: float
+    lon: float 
+    alt: int # feet
+    gspeed: int # knots
+    direction: int
+
+class MaxSightPoint(NamedTuple):
+    lat: float
+    lon: float
+    aircraft_side: AircraftSide 
+
+class CalculateMaxSightPointsResponse(NamedTuple):
+    right: MaxSightPoint
+    left: MaxSightPoint
